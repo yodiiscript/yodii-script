fe27970cba6693b443844ce3c5be16ffca654f26
thibaud <pernet-bissier@intechinfo.fr>
2015-06-03 15:09:11 +0200

    add function TryEscape and TryParse.

--- GUI/Watch.xaml.cs
+++ GUI/Watch.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.ObjectModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using Yodii.Script;
@@ -64,7 +65,7 @@ namespace GUI
             _varsCollection.Add( new VarData
             {
                 VarName = addVars.Text,
-                VarValue = Test.Object.ToString(),
+                VarValue = TryEscape(Test.Object.ToString()),
                 VarType = Test.Object.Type.ToString(),
             } );
         }
@@ -76,29 +77,114 @@ namespace GUI
         
     }
 
-    private void Change_Value( object sender, RoutedEventArgs e )
-    {
-        
-        //_engine.ScopeManager.FindByName(Watche.)
-    }
+    
 
     private void TextBox_KeyDown( object sender, System.Windows.Input.KeyEventArgs e )
     {
         if( e.Key == System.Windows.Input.Key.Enter )
         {
-           
+            
             TextBox tb = (TextBox)sender;
             var a = tb.Text;
+
+
+
+
             GUI.VarData obj2 = tb.DataContext as GUI.VarData;
             RefRuntimeObj O = _engine.ScopeManager.FindByName( obj2.VarName ).Object;
-            O.Value = new JSEvalNumber( double.Parse(a) );
+           
+            RuntimeObj result;
+            bool error;
+            result = TryParse(a, out error);
+            if( result == null && error == true )
+            {
+                MessageBoxResult popUp = MessageBox.Show( "Invalid Input" );
+                tb.Text = obj2.VarValue;
+               
+                 
+            } else if (result != null)
+            {
+                O.Value = result;
+            }
+
            
             
+           
+          
+
         }
+
+        
+
     }
 
-   
-  }
+    private string TryEscape( string a )
+    {
+        StringBuilder MyString = new StringBuilder();
+       
+
+        for( int cmpt = 0; cmpt < a.Length; cmpt++ )
+        {
+
+           
+           switch( a[cmpt] )
+            {
+                case '"': MyString.Append( '\\' ); MyString.Append( '"' ); break;
+                case '\\': MyString.Append( '\\'); break;
+                case '\n': MyString.Append( "\\n" ); break;
+                case '\t': MyString.Append( "\\t" ); break;
+                case '\r': MyString.Append( "\\r" ); break;
+                case '\b': MyString.Append( "\\b" ); break;
+                case '\v': MyString.Append( "\\v" ); break;
+                case '\f': MyString.Append( "\\f" ); break;
+                default: MyString.Append( a[cmpt]); break;
+            }
+        }
+
+            return MyString.ToString();
+    }
+
+
+    private RuntimeObj TryParse( string a, out bool hasError )
+    {
+        hasError = false;
+        RuntimeObj result;
+        var j = new JSTokenizer( a );
+        string sValue = j.ReadString();
+        if( sValue != null )
+        {
+            result = _engine.Context.CreateString( sValue );
+        }
+        else if( j.IsNumber )
+        {
+            double dValue = j.ReadDouble();
+            result = _engine.Context.CreateNumber( dValue );
+        }
+        else if( j.MatchIdentifier( JSSupport.TrueString ) )
+        {
+            result = JSEvalBoolean.True;
+        }
+        else if( j.MatchIdentifier( JSSupport.FalseString ) )
+        {
+            result = JSEvalBoolean.True;
+        }
+        else if( j.MatchIdentifier( "null" ) )
+        {
+            result = RuntimeObj.Null;
+        }
+        else if( j.MatchIdentifier( "undefined" ) )
+        {
+            result = RuntimeObj.Undefined;
+        }
+        else
+        {
+            return null;
+        }
+        if( j.IsEndOfInput ) return result;
+        hasError = true;
+        return null;
+    }
+  }  
 
   public class VarData
   {
